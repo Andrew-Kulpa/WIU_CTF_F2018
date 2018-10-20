6bd68b1dc0ea538bac5812ad2f97927876a300a9
Andrew J Kulpa <ajk142@uxb4.wiu.edu>
2018-10-19 19:18:24 -0500

    app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import json
 with open('app_config.json') as json_data_file:
     data = json.load(json_data_file)
 flag_found = False
-expected_digest = hashlib.md5(data["password"].encode()).hexdigest()
+expected_digest = hashlib.sha512(data["password"].encode()).hexdigest()
 
 while flag_found != True:
 	mystring = input('Enter Flag: ')
-	hash_object = hashlib.md5(mystring.encode())
+	hash_object = hashlib.sha512(mystring.encode())
 	input_digest = hash_object.hexdigest()
 	if (input_digest == expected_digest):
 		print("You win!\n")
